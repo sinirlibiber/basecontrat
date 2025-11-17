3ecf5b2904287d10d24bac8dbf61ec25258a003c
gumusbey <98549582+sinirlibiber@users.noreply.github.com>
2025-11-18 01:36:59 +0300

    Create SimpleStorage.sol

---
+++ src/contracts/SimpleStorage.sol
@@ -0,0 +1,22 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.0;
+
+contract SimpleStorage {
+    uint256 private storedData;
+    address public owner;
+
+    event DataStored(uint256 data, address indexed by);
+
+    constructor() {
+        owner = msg.sender;
+    }
+
+    function set(uint256 x) public {
+        storedData = x;
+        emit DataStored(x, msg.sender);
+    }
+
+    function get() public view returns (uint256) {
+        return storedData;
+    }
+}
